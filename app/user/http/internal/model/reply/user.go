19d6f53073833ff193362f3f982304ca0c9a9b4c
笑脸带眼红 <202606540@qq.com>
2024-04-14 17:25:10 +0800

    更新用户信息回显

--- app/user/http/internal/model/reply/user.go
+++ app/user/http/internal/model/reply/user.go
@@ -1,4 +1,13 @@
 package reply
 
+// UserInfoReply  用户信息
 type UserInfoReply struct {
+	Id        int64  `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	Avatar    string `json:"avatar"`
+	Male      int64  `json:"male"`
+	CreatedAt string `json:"createdAt"`
+	UpdatedAt string `json:"updatedAt"`
 }
